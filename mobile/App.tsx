2803f21a1a2e78daee981e53c841ec1c0f2c1f42
Felipe Martins <felipemjesus@outlook.com>
2022-09-12 18:59:51 -0300

    NLW eSports - Stage 1/6 - Base Building - Mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Hello React Native!
+      </Text>
+      <Button title='Send 1'></Button>
+      <Button title='Send 2'></Button>
+      <Button title='Send 3'></Button>
+      <Button title='Hello World'></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
